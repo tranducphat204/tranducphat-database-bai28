febc851b90afb3b87f4044b2f1854bc20aaa9eb0
phatdeptraiii <vitphat350@gmail.com>
2024-03-13 22:43:22 +0700

    .

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+// use DB;
+
+class UserController extends Controller
+{
+    public function index()
+    {
+        $title = 'Danh sách người dùng';
+        $users = DB::select('SELECT * from users ORDER BY create_at DESC');
+        return view('client.users.lists', compact('title', 'users'));
+    }
+}
---
+++ resources/views/clients/add.blade.php
@@ -0,0 +1,36 @@
+@extends('layouts.client')
+@section('titlet')
+{{$title}}
+@endsection
+@section ('content')
+<h1> Thêm sản phẩm </h1>
+<form action="" method="post">
+    @if ($errors->any())
+    <div class="alert alert-danger text-center">
+        {{$errorMessage}}
+    </div>
+    @endif
+    <div class="mb-3">
+        <label for="">Tên sản phẩm</label>
+        <input type="text" class="form-control" name="product_name" placeholder="tên sản phẩm..." value="{{old('product_name')}}">
+        @error('product_name')
+        <span style="color:red;">
+            {{$message}}
+        </span>
+        @enderror
+    </div>
+    <div class="mb-3">
+        <label for="">Giá sản phẩm</label>
+        <input type="text" class="form-control" name="product_price" placeholder="giá sản phẩm..." value="{{old('product_price')}}">
+        @error('product_price')
+        <span style="color:red;">
+            {{$message}}
+        </span>
+        @enderror
+    </div>
+    <button type="submit" class="btn btn-primary">Thêm mới</button>
+    @csrf
+</form>
+@endsection
+@section ('css')
+@endsection
